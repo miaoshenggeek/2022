d81621fbb4c17840881181045748f7e602572341
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-06 16:44:07 -0800

    Time: 293 ms (19.77%), Space: 22.3 MB (58.86%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -10,10 +10,10 @@ class Solution:
             if not root:return
             if l<=root.val<=h:
                 res.append(root.val)
-            helper(root.right,l,h)
-            helper(root.left,l,h)
-            #if root.val<l:helper(root.right,l,h)
-            #if root.val>h:helper(root.left,l,h)
+                helper(root.right,l,h)
+                helper(root.left,l,h)
+            if root.val<l:helper(root.right,l,h)
+            if root.val>h:helper(root.left,l,h)
         res=[]
         helper(root,low,high)
         return sum(res)
