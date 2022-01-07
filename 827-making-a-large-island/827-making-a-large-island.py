28a85bab1af42f829970cc0dc55b8071eaabf10f
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-06 22:02:31 -0800

    Time: 5935 ms (8.81%), Space: 26.9 MB (40.60%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.py
+++ 827-making-a-large-island/827-making-a-large-island.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         n=len(grid)
         
@@ -35,9 +35,39 @@ class Solution:
                 if grid[i][j]==0:
                     can={grid[nr][nc] for nr,nc in neighbors(i,j) if grid[nr][nc]>1}
                     ans=max(ans,1+sum(rt[i] for i in can))
-            
-                
-        #print(rt)            
+                      
+        return ans'''
+class Solution(object):
+    def largestIsland(self, grid):
+        N = len(grid)
+
+        def neighbors(r, c):
+            for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                if 0 <= nr < N and 0 <= nc < N:
+                    yield nr, nc
+
+        def dfs(r, c, index):
+            ans = 1
+            grid[r][c] = index
+            for nr, nc in neighbors(r, c):
+                if grid[nr][nc] == 1:
+                    ans += dfs(nr, nc, index)
+            return ans
+
+        area = {}
+        index = 2
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c] == 1:
+                    area[index] = dfs(r, c, index)
+                    index += 1
+
+        ans = max(area.values() or [0])
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c] == 0:
+                    seen = {grid[nr][nc] for nr, nc in neighbors(r, c) if grid[nr][nc] > 1}
+                    ans = max(ans, 1 + sum(area[i] for i in seen))
         return ans
 
     
