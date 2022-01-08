8603397024f39a384e61f50ea39e85f80fcc0ca7
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-07 23:34:43 -0800

    Time: 28 ms (87.58%), Space: 14.4 MB (18.63%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -10,11 +10,12 @@ class Solution:
             nonlocal res
             if not root:return
             path=path*10+root.val
-            if not root.right and not root.left:
-                res+=path
-                
+                          
             dfs(root.left,path)    
             dfs(root.right,path)
+            
+            if not root.right and not root.left:
+                res+=path
             
         path=0
         res=0
