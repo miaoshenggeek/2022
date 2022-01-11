a13eecb3d34e4030297f9c75b67504427dd2b8e8
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-11 11:18:57 -0800

    Time: 458 ms (19.10%), Space: 27.1 MB (22.36%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, h: int, w: int, hc: List[int], vc: List[int]) -> int:
+        hc.sort()
+        vc.sort()
+        hc=[0]+hc+[h]
+        vc=[0]+vc+[w]
+        #print(hc,vc)
+        hh=max(i[0]-i[1] for i in zip(hc[1:],hc))
+        ww=max(i[0]-i[1] for i in zip(vc[1:],vc))
+        #print(hh,ww)
+        return hh*ww%(1000000007)
