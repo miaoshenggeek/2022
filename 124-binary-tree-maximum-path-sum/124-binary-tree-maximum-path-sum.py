4362cfa2c9bc33faabd3ead3977a03bf25f748f8
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-11 10:43:58 -0800

    Time: 158 ms (6.34%), Space: 21.7 MB (40.13%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -5,30 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    def __init__(self):
-        self.max_sum = None
-    
-    def recursive_sum(self, node):
-        if node:
-            max_sum_left = self.recursive_sum(node.left)
-            node_val = node.val
-            max_sum_right = self.recursive_sum(node.right)
-            
-            max_sum_node = max(node_val, node_val + max_sum_left, node_val + max_sum_right)
-            self.max_sum = max(self.max_sum, max_sum_node, node_val + max_sum_left + max_sum_right)
-            
-            return max_sum_node
-        return 0
-
-    
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # SOL1: Recursive Solution
-        # TC = O(N) and SC = O(N) if tree is left skewed (O(logN) if tree balanced)
-        self.max_sum = root.val
-        self.recursive_sum(root)
+            # naive: traverse once to make adjacency list, then find max subgraph
+            # leveraging subtrees...
+            # recurse down until leaf
+            # go up, return max(root,left+root,right+root) BUT check left, right, root+left+right against global max
+            max_sum = float('-inf')
+
+            def maxPathSumSubtree(root: Optional[TreeNode]) -> int:
+                if not root.left and not root.right:
+                    return root.val
+                left_val = maxPathSumSubtree(root.left) if root.left else float('-inf')
+                right_val = maxPathSumSubtree(root.right) if root.right else float('-inf')
+                dfs_max = max(root.val, left_val + root.val, right_val + root.val)
+
+                nonlocal max_sum
+                max_sum = max(max_sum, dfs_max, left_val, right_val, root.val + left_val + right_val)
+
+                return dfs_max
+
+            return max(maxPathSumSubtree(root), max_sum)
         
-        return self.max_sum
             
             
         
