d054ff3cd233ae5bfb0a145818997673f36ee2d5
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-13 13:51:56 -0800

    Time: 48 ms (97.66%), Space: 14.3 MB (67.11%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,5 +1,5 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counter = collections.Counter(words)    # O(n) space and time
-        return heapq.nsmallest(k, counter, key=lambda word:(~counter[word], word))
+        return heapq.nsmallest(k, counter, key=lambda word:(-counter[word], word))
         #return [j[0] for j in sorted(counter.items(),key=lambda i: (-i[1],i[0]))][:k]  #O (n log n)
