dfe9ad1f9cf6f7330b55b030393a60fe9edab416
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-13 10:43:15 -0800

    Time: 60 ms (50.96%), Space: 14.5 MB (39.43%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,6 +1,7 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        c=collections.Counter(words)
+        """c=collections.Counter(words)
+        #return heapq.nlargest(k, c.keys(), key = c.get)#no sorting result
         q=[]
         dic=defaultdict(list)
         for word,count in c.items():
@@ -13,7 +14,19 @@ class Solution:
             idx=heapq.heappop(q)
             res.extend(sorted(dic[-idx]))
             if len(res)>=k:return res[:k]
+        return res"""
+        #bucket sort
+        bucket=[[] for _ in range(len(words)+1)]  #each pair is count for each word
+        c=collections.Counter(words)
+        for word,count in c.items():
+            bucket[count].append(word)
+        res=[]
+        for i,wordlist in enumerate(bucket[::-1]):
+            res.extend(sorted(wordlist))
+            if len(res)>=k:return res[:k]
         return res
+        
+            
         
             
         
