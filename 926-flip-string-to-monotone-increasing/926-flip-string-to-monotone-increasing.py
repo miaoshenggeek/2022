fe46528be46f6cac798eef40216495638573a430
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-13 23:06:59 -0800

    Time: 730 ms (8.51%), Space: 18.7 MB (26.64%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -1,7 +1,8 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        #flip right 0 to 1
-        #flip left 1 to 0
+        #那我们只要一直记录着当前位置的前面有多少个1，后面有多少个0即可！
+        #注意：可能存在最后全为0的情况（如示例3），那么我们是选不到某个’1’作为开头的，
+        #所以我们先将全部翻转成’0’所花费的次数作为初始默认次数，然后和我们每次计算的结果比较即可。
         if not "0" in s or not "1" in s or len(s)==1: return 0
         n=len(s)
         P=[0]*(n+1)
@@ -10,5 +11,17 @@ class Solution:
         #print(P)
         ans=n
         for i in range(n+1):
-            ans=min(ans,n-i-P[n]+2*P[i])
+            ans=min(ans,n-i-(P[n]-P[i])+P[i])
         return ans
+    
+        '''n = len(s)
+        cnt0 = s.count('0')
+        cnt1 = 0
+        res = n - cnt0
+        for i in range(n):
+            if s[i] == '0': 
+                cnt0 -= 1
+            elif s[i] == '1':
+                res = min(res, cnt1+cnt0)
+                cnt1 += 1
+        return res'''
