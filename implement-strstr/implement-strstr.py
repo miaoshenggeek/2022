2f66608a317ecb9bdc816d87b89b481c8910420a
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-14 21:26:05 -0800

    Time: 1335 ms, Memory: 14.6 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -2,10 +2,10 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle=="":return 0
         if haystack=="":return -1
-        #use memoryview to not copy when slice string
+        #Using memoryview to slice without copying
         haystack = memoryview(bytes(haystack, 'ascii'))
         needle = memoryview(bytes(needle, 'ascii'))
-
+        # slicing lists has a time-complexity of O(k), where "k" is the length of the slice
         m=len(needle)
         n=len(haystack)
         for i in range(n):
