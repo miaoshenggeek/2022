74fbeb7ff342ba2f2fd42c5720730bd491a277a6
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-17 13:39:01 -0800

    Time: 159 ms (44.28%), Space: 15.3 MB (61.69%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
@@ -10,13 +10,14 @@ class Solution:
             mid=lb+(ub-lb)//2
             a=bisect_left(arr,mid,0,n)
             can=sum(arr[:a])+mid*(n-a)
-            #print(can)
             if can==t:
                 ub=mid
             elif can<t:
                 lb=mid+1
             elif can>t:
                 ub=mid
+        #print(can)
+        #always return lb , lb>=t
         a=bisect_left(arr,lb,0,n)
         can=sum(arr[:a])+lb*(n-a)
         b=bisect_left(arr,lb-1,0,n)
