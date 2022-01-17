4721a8ecabc160935dfe98dd66ebe21a119702b7
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-17 14:56:47 -0800

    Time: 243 ms (17.41%), Space: 15.3 MB (90.05%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
@@ -1,44 +1,31 @@
 class Solution:
-    def findBestValue(self, arr: List[int], t: int) -> int:
-        '''n=len(arr)
-        # find closest value so n*v->t
-        # t break arr to <t and ==t
-        arr.sort()
-        ub=max(arr)
-        lb=1
-        while lb<ub:
-            mid=lb+(ub-lb)//2
-            a=bisect_left(arr,mid,0,n)
-            can=sum(arr[:a])+mid*(n-a) #F(lb)
-            if can==t:
-                ub=mid
-            elif can<t:
-                lb=mid+1 #
-            elif can>t:
-                ub=mid
-        #print(can)
-        #always return lb , F(lb)>=t
-        #compare F(lb) with F(lb-1),where F(lb-1)<t
-        a=bisect_left(arr,lb,0,n)
-        can=sum(arr[:a])+lb*(n-a)
-        b=bisect_left(arr,lb-1,0,n)
-        can1=sum(arr[:b])+(lb-1)*(n-b)
-        return lb if abs(can-t)<abs(can1-t) else lb-1
-        '''
-        arr.sort()
-        s, n = 0, len(arr)
+    def getRes(self,arr,t):
+        nums = [t if num >= t else num for num in arr]
+        return sum(nums)
+    
+    def findBestValue(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 1
+        h = max(arr)
         
-        for i in range(n):
-            # ans is best to replace everything from i and on. 
-            ans = round((t - s)/n)   #use round to return closest integer to float-四舍五入
-            # if this number is smaller than i-th number
-            # then return this number, as the next one only makes it bigger
-            #print(ans)
-            if ans <arr[i]: return ans 
-            s += arr[i]
-            n -= 1
-            
-        return arr[-1]
+        while l <= h:
+            mid = (h-l)//2 + l
+            curr = self.getRes(arr,mid)
+            if curr == target:
+                return mid
+            elif curr < target:
+                l = mid+1
+            else:
+                h = mid-1
+        #end loop l<h or l==h
+        #print(l,h,mid)
+        if abs(self.getRes(arr,l) - target) < abs(self.getRes(arr,h) - target):
+            return l
+        return h
             
         
         
