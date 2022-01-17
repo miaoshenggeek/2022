47b28e31f4c3316971feb0fb602f13360864256c
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-17 13:29:22 -0800

    Time: 100 ms (73.13%), Space: 15.3 MB (90.05%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findBestValue(self, arr: List[int], t: int) -> int:
+        n=len(arr)
+        # find closest value so n*v->t
+        # t break arr to <t and ==t
+        arr.sort()
+        ub=max(arr)
+        lb=1
+        while lb<ub:
+            mid=lb+(ub-lb)//2
+            a=bisect_left(arr,mid,0,n)
+            can=sum(arr[:a])+mid*(n-a)
+            #print(can)
+            if can==t:
+                ub=mid
+            elif can<t:
+                lb=mid+1
+            elif can>t:
+                ub=mid
+        a=bisect_left(arr,lb,0,n)
+        can=sum(arr[:a])+lb*(n-a)
+        b=bisect_left(arr,lb-1,0,n)
+        can1=sum(arr[:b])+(lb-1)*(n-b)
+        return lb if abs(can-t)<abs(can1-t) else lb-1
+        
+            
+        
+        
+        
