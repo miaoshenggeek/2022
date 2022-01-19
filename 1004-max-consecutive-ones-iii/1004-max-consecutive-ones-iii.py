eb1071902163e5208125087bc14c4ccebef39412
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-19 12:45:16 -0800

    Time: 760 ms (35.52%), Space: 14.7 MB (91.00%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,12 +1,11 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        start=0
+        '''start=0
         end=1
         cur=1-nums[start]
         res=k
         n=len(nums)
         while end<n:
-           
             while (cur==k and nums[end]==0) or cur>k:
                 cur=cur-(1-nums[start])
                 start+=1   
@@ -15,5 +14,24 @@ class Solution:
             end+=1
             res=max(res,end-start)
             #print(cur,res,end,start)
-        return res
+        return res'''
+        left = right = 0
+
+        for right in range(len(nums)):
+            # if we encounter a 0 the we decrement K
+            if nums[right] == 0:
+                k -= 1
+            # else no impact to K
+
+            # if K < 0 then we need to move the left part of the window forward
+            # to try and remove the extra 0's
+            if k < 0:
+                # if the left one was zero then we adjust K
+                if nums[left] == 0:
+                    k += 1
+                # regardless of whether we had a 1 or a 0 we can move left side by 1
+                # if we keep seeing 1's the window still keeps moving as-is
+                left += 1
+
+        return right - left + 1
         
