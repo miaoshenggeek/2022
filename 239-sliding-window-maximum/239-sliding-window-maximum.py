37934874bf435fc9ef549848b197befdeb9f4101
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-21 22:43:02 -0800

    Time: 1688 ms (89.02%), Space: 29.4 MB (91.85%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,29 +1,35 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        #You want to ensure the deque window only has decreasing elements. 
-        #That way, the leftmost element is always the largest.
+        #Ensure the deque window only has decreasing elements. 
+        #So the leftmost element is always the largest.
         q=deque()
         res=[]
         for i,v in enumerate(nums):
             
-            if i<k:
-                if not q:q.append(i)
-                else:
-                    while q and nums[q[-1]]<=v:
-                        q.pop()
-                    q.append(i)
-                if i==k-1:res.append(nums[q[0]])
-                continue
-            
             while q and nums[q[-1]]<=v:
                 q.pop()
+                
             q.append(i)
             
             if q[0]<=i-k:q.popleft()
-            m=q[0]    
-            res.append(nums[m])
-        return res
                 
+            if i>=k-1:res.append(nums[q[0]])
+            
+        return res
+        '''
+        d = collections.deque()
+        out = []
+        
+        for i, n in enumerate(nums):
+            while d and nums[d[-1]] < n:
+                d.pop()
+                
+            d.append(i)
+            if d[0] == i - k:  # 存了K+1个元素了
+                d.popleft()
+            if i >= k - 1:
+                out.append(nums[d[0]])
+        return out'''
                     
         
             
