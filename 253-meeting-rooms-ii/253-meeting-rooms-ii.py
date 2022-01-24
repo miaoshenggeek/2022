5fbaf2eb4fbd15e0ae10db1153c6575b56a691d5
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-23 22:43:48 -0800

    Time: 80 ms (68.43%), Space: 17.7 MB (24.20%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -5,7 +5,6 @@ class Solution:
         #sort by start time
         #maintain a min heap of end-time
         q=[]
-        res=0
         heapq.heapify(q)
         for i in range(len(A)):
             if q:
@@ -14,7 +13,6 @@ class Solution:
                     heapq.heappop(q)
             heapq.heappush(q,A[i][1])
             #print(q)
-            res=max(res,len(q))
-        return res
+        return len(q)
     #[[6,15],[13,20],[6,17]]
     
