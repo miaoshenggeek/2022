96496d3c1cb8d2c433126de7416181e84f2b97db
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-24 00:08:45 -0800

    Time: 1564 ms (65.00%), Space: 60.9 MB (5.69%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -3,18 +3,29 @@ class Solution:
         #count overlapped balloons as one. 
         #count the number of unoverlapped ballons
         #sort by start
-        A.sort(key=lambda i:i[0])
+        '''A.sort(key=lambda i:i[0])
         #print(A)
         c=1
-        start=A[0][0]#1
-        end=A[0][1]#6
+        start=A[0][0]
+        end=A[0][1]
         for i,j in A[1:]:
             if i<=end:
-                start=i#2 
-                end=min(j,end) #6             
+                start=i
+                end=min(j,end)          
             else:
                 c+=1
                 start=i
                 end=j
             #print(q)
-        return c
+        return c'''
+        
+        arrows   = 0
+        prev_end = 0
+        
+        for start, end in sorted(A, key=lambda i:i[1]):
+            if not prev_end or start > prev_end:
+                arrows += 1
+                prev_end = end
+        
+        return arrows
+    
