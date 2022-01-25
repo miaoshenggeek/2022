be7b053d0837ab1476a6d7493e1e39b6c83502a7
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-24 16:02:33 -0800

    Time: 110 ms (21.47%), Space: 14.5 MB (11.50%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,9 +5,8 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        '''def helper(l1,l2,l3,carry=0):
+        def helper(l1,l2,l3,carry=0):
             if not l1 and not l2 and not carry: return
-            
             new=ListNode()
             l3.next=new
             a=l1.val if l1 else 0
@@ -20,31 +19,5 @@ class Solution:
             return new
         prev=ListNode()
         prev.next=helper(l1,l2,prev,0)
-        return prev.next'''
-        if l1 is None: return l2
-        if l2 is None: return l1
-        l3=ListNode(0)
-        cur=l3  
-        pas=0
-        while l1 and l2 :
-            cur.next=ListNode((l1.val+l2.val+pas)%10)
-            pas=(l1.val+l2.val+pas)//10
-            cur=cur.next
-            l1=l1.next
-            l2=l2.next
-        while l1 :
-            cur.next=ListNode((l1.val+pas)%10)
-            pas=(pas+l1.val)//10
-            cur=cur.next
-            l1=l1.next
-        while l2 :
-            cur.next=ListNode((l2.val+pas)%10)
-            pas=(pas+l2.val)//10
-            cur=cur.next
-            l2=l2.next
-        if pas==1:
-            cur.next=ListNode(1)
-        return l3.next
-    
-
-            
+        return prev.next
+        
