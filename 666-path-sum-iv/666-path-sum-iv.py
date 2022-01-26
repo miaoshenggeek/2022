1c5e20d473e09ae710f52c30c4abb1831004e8dd
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-26 11:17:42 -0800

    Time: 41 ms (40.49%), Space: 14.2 MB (87.80%) - LeetHub

---
+++ 666-path-sum-iv/666-path-sum-iv.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pathSum(self, nums: List[int]) -> int:
+        dic = collections.defaultdict(int)
+        # Create a dict of depth and pos
+        for num in nums:
+            depth, pos = num//100, (num//10)%10
+            val = num % 10
+            # At each level, store the sum of all the previous nodes covered
+            dic[(depth, pos)] = dic[(depth-1, (pos+1)//2)] + val
+            
+        res = 0
+        for depth,pos in dic.keys():
+            # Since the leaf nodes contain sum of the entire path from root to each leaf nodes, just check whether it is a leaf node or not and then add up all the leaf nodes
+            if (depth+1, pos*2-1) not in dic and (depth+1,pos*2) not in dic:
+                res += dic[depth,pos]
+        return res
