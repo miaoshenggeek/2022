a4c8906ad7d9c6ced2de63fa2d0912e205421366
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-28 19:33:46 -0800

    Time: 1010 ms (5.53%), Space: 14.8 MB (70.76%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -4,12 +4,12 @@ class Solution:
         cur=Counter()  #counter
         l=0
         for r in range(len(s)):
-            #if s[r] in cur:
-            cur[s[r]]+=1
-            
+            if s[r] in cur:
+                cur[s[r]]+=1
+            else:
+                cur[s[r]]=1
             while len(cur)>2 and l<r:
                 cur[s[l]]-=1
-                +cur
                 if cur[s[l]]==0:del cur[s[l]]
                 l+=1
             
