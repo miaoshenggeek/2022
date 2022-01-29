22b43d438b4c6ec71c7d403b98180fe08812c733
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-28 19:46:51 -0800

    Time: 92 ms (56.98%), Space: 14.2 MB (95.35%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -3,16 +3,14 @@ class Solution:
         #if k==0:return 0
         i,j=0,0
         res=0
-        cur=0
         seen=defaultdict(int) #cnt
         
         while j<len(s):
             seen[s[j]]+=1
-            if seen[s[j]]==1:cur+=1
             j+=1
-            while i<j and cur>k:
+            while i<j and len(seen)>k:
                 seen[s[i]]-=1
-                if seen[s[i]]==0:cur-=1
+                if seen[s[i]]==0:del seen[s[i]]
                 i+=1
             #print(cur,i,j)
             res=max(res,j-i)
