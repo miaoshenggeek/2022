323d4408858290d708fdfb8363ecd64884922218
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-31 10:03:26 -0800

    Time: 24 ms (97.46%), Space: 13.9 MB (96.95%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        adj={'2':["a","b","c"],
+             '3':['d','e','f'],
+             '4':['g','h','i'],
+             '5':['j','l','k'],
+             '6':['m','n','o'],
+             '7':['p','q','r','s'],
+             '8':['t','u','v'],
+             '9':['w','x','y','z']
+            }
+    
+        if not digits:return 
+        n=len(digits)
+        res=deque(adj[digits[0]])
+        for i in digits[1:]:
+            a=len(res)
+            for _ in range(a):
+                cur=res.popleft()
+                for j in adj[i]:
+                    res.append(cur+j)
+        return list(res)
+            
