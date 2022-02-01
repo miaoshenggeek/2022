c5acb33008cd98635ce0d4f698179241afb857c2
miaoshenggeek <58196591+miaoshenggeek@users.noreply.github.com>
2022-01-31 21:46:05 -0800

    Time: 655 ms (5.04%), Space: 70.8 MB (27.70%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canReach(self, arr: List[int], start: int) -> bool:
+    def bfs_canReach(self, arr: List[int], start: int) -> bool:
         if not 0 in arr:return False
         que=deque([start])
         seen=set()
@@ -8,9 +8,22 @@ class Solution:
             num=arr[idx]
             if num==0:return True
             if idx in seen:
-                continue 
-            
+                continue   
+                ###no need to exit here,
+                #continue will prevent dup and make sure que get empty finally
             if idx+num<len(arr):que.append(idx+num) 
             if idx-num>=0:que.append(idx-num) 
             seen.add(idx)
         return False
+    def canReach(self, arr: List[int], start: int) -> bool:
+        def helper(start):
+            if 0<=start<len(arr) and start not in seen:
+                if arr[start]==0:return True
+                seen.add(start)
+                return helper(start-arr[start]) or helper(start+arr[start])
+            return False
+        
+        seen=set()
+        return helper(start)
+        
+            
